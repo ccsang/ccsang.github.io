4a58f6d41389fbda85a7e7223f450e19c7b40b5e
qingyun <codingtsunami@gmail.com>
2017-03-22 22:16:25 +0800

    ok
    ok

--- js/test.js
+++ js/test.js
@@ -2,3 +2,15 @@ const log = (...args) => { console.log.apply(console, [ Date.now(), ...args] )}
 
 log(document.getElementsByClassName('user_info')[0])
 log(document.cookie)
+
+
+const xhr = new XMLHttpRequest()
+
+xhr.onreadystatechange   = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    log('wakaka ok')
+  }
+}
+
+xhr.open('get', '/check_login?r=0.7448267473768513&_=1490191688410')
+xhr.send()
